c7604c31930318e4a35563fe9d12174f74062473
gyaanantia <78275262+gyaanantia@users.noreply.github.com>
2024-05-02 16:32:50 -0500

    name experiments properly

--- onpolicy/scripts/train_patrolling_scripts/sd-no-skip-steps.py
+++ onpolicy/scripts/train_patrolling_scripts/sd-no-skip-steps.py
@@ -6,7 +6,7 @@ os.environ["WANDB__SERVICE_WAIT"] = "300"
 parser = get_config()
 all_args = parse_args([], parser)
 
-all_args.experiment_name = "search-and-deliver-increase-gnn-hops"
+all_args.experiment_name = "search-and-deliver-no-skip-steps"
 all_args.env_name = "search-deliver"
 all_args.user_name = "ideas-mas"
 
