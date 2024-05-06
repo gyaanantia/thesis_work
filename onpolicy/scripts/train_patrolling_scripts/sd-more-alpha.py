216ff83ffee9ef9138183bec60798bc6aee0c81d
gyaanantia <78275262+gyaanantia@users.noreply.github.com>
2024-05-06 00:56:29 -0500

    set up experiment for more visibility

--- onpolicy/scripts/train_patrolling_scripts/sd-more-alpha.py
+++ onpolicy/scripts/train_patrolling_scripts/sd-more-alpha.py
@@ -6,7 +6,7 @@ os.environ["WANDB__SERVICE_WAIT"] = "300"
 parser = get_config()
 all_args = parse_args([], parser)
 
-all_args.experiment_name = "search-and-deliver-more-alpha-movement-penalty-4-agents"
+all_args.experiment_name = "search-and-deliver-more-alpha-add-visibility"
 all_args.env_name = "search-deliver"
 all_args.user_name = "ideas-mas"
 
