30ade8e53e79b67d9fdffe9c60a46e9a4ad15a2c
gyaanantia <78275262+gyaanantia@users.noreply.github.com>
2024-05-10 03:15:08 -0500

    set up experiments with the nongraph observations

--- onpolicy/scripts/train_sd_scripts/sd-alpha-no-skip.py
+++ onpolicy/scripts/train_sd_scripts/sd-alpha-no-skip.py
@@ -6,7 +6,7 @@ os.environ["WANDB__SERVICE_WAIT"] = "300"
 parser = get_config()
 all_args = parse_args([], parser)
 
-all_args.experiment_name = "search-and-deliver-alpha-only-gnn"
+all_args.experiment_name = "search-and-deliver-alpha-gnn-with-nongraph"
 all_args.env_name = "search-deliver"
 all_args.user_name = "ideas-mas"
 
--- onpolicy/scripts/train_sd_scripts/sd-single.py
+++ onpolicy/scripts/train_sd_scripts/sd-single.py
@@ -6,7 +6,7 @@ os.environ["WANDB__SERVICE_WAIT"] = "300"
 parser = get_config()
 all_args = parse_args([], parser)
 
-all_args.experiment_name = "search-and-deliver-gnn-only"
+all_args.experiment_name = "search-and-deliver-single-gnn-with-nongraph"
 all_args.env_name = "search-deliver"
 all_args.user_name = "ideas-mas"
 
